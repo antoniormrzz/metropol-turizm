e977128103c211b93616643d6e5619c605b09717
slhozdemir <salihozdemir94@gmail.com>
2020-12-19 15:25:34 +0300

    breakpoints changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
   purge: [],
-  darkMode: 'media', // or false or 'class'
+  darkMode: "media", // or false or 'class'
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
