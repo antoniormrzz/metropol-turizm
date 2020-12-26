ddef2d2f43bacc555f67c2e9526d56593dfb3833
sepehr <sepehralizade@live.com>
2020-12-26 08:43:07 +0300

    added dirty slider to mobile home

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,8 +1,11 @@
 // for all modules, import tailwind and it's scss style
 import 'tailwindcss/tailwind.css';
 import './index.scss';
-
+import '@splidejs/splide/dist/css/splide.min.css';
 import lottie from 'lottie-web';
+import Splide from '@splidejs/splide';
+
+new Splide('#splide', { type: 'loop', autoplay: true }).mount();
 
 let animation = lottie.loadAnimation({
   container: document.getElementById('plane-container'),
