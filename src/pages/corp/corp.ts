c0746e757472dabc2f5c735cb4cb6ab39e48a2d9
ikbal <ikbal.bilginer@metropolsoftware.com>
2021-01-11 15:58:58 +0300

    corp glide slider

--- src/pages/corp/corp.ts
+++ src/pages/corp/corp.ts
@@ -1,3 +1,15 @@
 // for all modules, import tailwind and it's scss style
 import "tailwindcss/tailwind.css";
 import './corp.scss';
+import Glide from '@glidejs/glide';
+import '@glidejs/glide/dist/css/glide.core.min.css';
+import '@glidejs/glide/dist/css/glide.theme.min.css';
+
+var glideMulti1 = new Glide('.multi1', {
+    type: 'carousel',
+    autoplay: 3500,
+    perView: 3
+  });
+
+
+glideMulti1.mount();
