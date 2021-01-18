7cf1f69a5db72a8529b5905e67dbc5830763b83c
ikbal <ikbal.bilginer@metropolsoftware.com>
2021-01-18 14:02:13 +0300

    travel&corp scaffold

---
+++ src/pages/travel/travel.ts
@@ -0,0 +1,6 @@
+// for all modules, import tailwind and it's scss style
+import "tailwindcss/tailwind.css";
+import './travel.scss';
+import menuHandler from "../../global/src/menu";
+
+menuHandler();
