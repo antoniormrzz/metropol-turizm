d1d5289b37b75af9df15744a6e2cb8cb5891bc90
salih <salih.ozdemir@metropolsoftware.com>
2021-01-28 10:56:19 +0300

    last changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
       content: ["20px", "32px"],
       title: ["60px", "72px"],
       icon: ["25px", "32px"],
-      bigTitle: ["64px", "72px"],
+      bigTitle: ["54px", "72px"],
       titlemobile: ["40px", "52px"],
       base: "1rem",
       lg: "1.125rem",
