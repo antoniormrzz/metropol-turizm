acc57d720c459a0592ffda5b3ea58a292f2ac866
salih <salih.ozdemir@metropolsoftware.com>
2021-02-17 11:57:59 +0300

    balkans detail

--- src/pages/tour-detail/tour-detail.ts
+++ src/pages/tour-detail/tour-detail.ts
@@ -1,13 +1,26 @@
 // for all modules, import tailwind and it's scss style
 import "tailwindcss/tailwind.css";
 import "./tour-detail.scss";
-import ejs from "ejs/ejs.min.js";
-import $ from "jquery";
+import balkanTours from "../../constants/balkan-tours";
 
-import data from "../../views/object";
-
-// let html = new ejs({url: '../../views/tour_template'}).render(data)
-let html = ejs.renderFile("../../views/tour_template.ejs", { data: 1 });
-// <%- include('components/header.ejs', {data: 'data'}); %>
-
-$("#output").html(html);
+new Vue({
+  el: "#app",
+  data: {
+    tourData: balkanTours,
+    selectedTour: balkanTours[0],
+    showDrawer: false,
+  },
+  computed: {
+    cutText: function () {
+      return this.selectedTour.days[0].text.substring(0, 116) + "...";
+    },
+  },
+  methods: {
+    selectTour: function (index) {
+      this.selectedTour = this.tourData[index];
+    },
+    toggleDrawer: function () {
+      this.showDrawer = !this.showDrawer;
+    },
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 module.exports = {
-  purge: ['./src/pages/**/*.ejs','./src/views/**/*.ejs'],
+  purge: ["./src/pages/**/*.ejs", "./src/views/**/*.ejs"],
   darkMode: "media", // or false or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        content: ["20px", "32px"],
+        title: ["60px", "72px"],
+        icon: ["25px", "32px"],
+        bigTitle: ["54px", "72px"],
+        titlemobile: ["40px", "52px"],
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+      },
+    },
     screens: {
       sm: "640px",
       md: "768px",
       lg: "1024px",
       xl: "1280px",
-    },
-    fontSize: {
-      content: ["20px", "32px"],
-      title: ["60px", "72px"],
-      icon: ["25px", "32px"],
-      bigTitle: ["54px", "72px"],
-      titlemobile: ["40px", "52px"],
-      base: "1rem",
-      lg: "1.125rem",
-      xl: "1.25rem",
-      "2xl": "1.5rem",
-      "2xl": "1.5rem",
-      "3xl": "1.875rem",
-      "4xl": "2.25rem",
     },
   },
   variants: {
